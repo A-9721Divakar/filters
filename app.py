9c399fc702ec3d1eda4ba1f1b869395190735bd0
A-9721Divakar <155224597+A-9721Divakar@users.noreply.github.com>
2024-10-02 23:27:26 +0530

    app.py

--- app.py
+++ app.py
@@ -93,5 +93,5 @@ def open_camera():
 if __name__ == '__main__':
     app.run(debug=True)
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
